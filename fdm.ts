1e924175b9ddb11933892e1e4d1d3bf42c3cddea
Conard <conard_ferenc@outlook.com>
2023-08-13 16:05:35 +0800

    :bug: (fdm.ts) 修复打包报错

--- fdm.ts
+++ fdm.ts
@@ -15,7 +15,7 @@ program
   .option('-v3 --vue3', 'create a vue3 project')
   .description('create a new vue project')
   .action(async (option) => {
-    const { createProject } = await import('./commands/create.ts')
+    const { createProject } = await import('./commands/create')
     return createProject(option)
   })
 
@@ -23,7 +23,7 @@ program
   .command('list')
   .description('List the templateList')
   .action(() => {
-    import('./commands/list.ts')
+    import('./commands/list')
   })
 
 program
