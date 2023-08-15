5ba7cb21aa43ed8636d9f20a7cd212bf355d3265
Ned <15041567431@126.com>
2023-08-15 20:21:48 +0800

    :poop: (showTable.ts)
    增加终端列表显示函数

---
+++ util/showTable.ts
@@ -0,0 +1,25 @@
+import Table  from 'cli-table'
+import logSymbols  from 'log-symbols';
+
+const table = new Table({
+  head: ['模板名'],
+  style: {
+    head: ['green']
+  }
+})
+
+export function showTable (tempList: string[] ) {
+  const list = tempList
+  if (list.length > 0) {
+    list.forEach((key) => {
+      table.push([key])
+      if (table.length === list.length) {
+        console.log(table.toString())
+        process.exit()
+      }
+    })
+  } else {
+    console.log(logSymbols.info,'模板库中暂无模板，请等待官方添加哦~')
+    process.exit()
+  }
+}
