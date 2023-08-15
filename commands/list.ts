68f58ee51760e012eda5576047a6fb1de8bcebfd
Ned <15041567431@126.com>
2023-08-15 20:20:49 +0800

    :sparkles: (list.ts)
    增加list命令

--- commands/list.ts
+++ commands/list.ts
@@ -1,6 +1,21 @@
 // 查看提供好的模板项目清单
 import fse from 'fs-extra'
+import { resolve } from 'path'
+import ora  from 'ora'
+import logSymbols  from 'log-symbols';
+import { showTable } from '@util/showTable'
+const spinner = ora('正在为你拼命加载...\n').start();
 
-fse.readdir('../lib/template',(err,files) => {
-    console.log(files)
+const dir = resolve('./lib/template')
+
+fse.readdir(dir, (err,files) => {
+  // 错误处理
+  if(err){
+    spinner.fail('似乎发生了什么错误');
+    console.log(logSymbols.error,err)
+  }else{
+    spinner.succeed('加载成功，请查看结果');
+    // console.log(files)
+    showTable(files)
+  }
 })
